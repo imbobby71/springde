57ecc7d2e9afac3c4762d729b5bf53ce02675427
imbobby71 <imbobby71@gmail.com>
2024-11-18 16:06:19 +0900

    20241118

---
+++ src/main/java/com/office/springde/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.office.springde.config;
+
+public class AppConfig {
+	
+	final static public String server_name_local = "local";
+	final static public String server_name_dev = "dev";
+	final static public String server_name_prod = "prod";
+
+}
